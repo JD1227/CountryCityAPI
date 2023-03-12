a0119336bb1dc9e285566e4db636b0b9468a99ce
Jaita Das <101314515+JD1227@users.noreply.github.com>
2023-03-12 13:09:07 +0530

    This is the view page
    where HelloWorld component is imported

---
+++ HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <nav>Welcome!</nav>
+
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  padding: 30px;
+  font-size: 25px;
+}
+</style>
